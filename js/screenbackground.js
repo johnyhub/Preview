7bffb2c6383cecd2e24fc1837d5de3abace2a40c
johnyhub <31032148+johnyhub@users.noreply.github.com>
2017-09-26 13:57:45 +0200

    ch2

--- js/screenbackground.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-              $("#high").click(function(){
-                  
-                  $(".screen-content").removeClass().addClass("screen-content screen-img1");
-               });
-        $("#easy").click(function(){
-                  $(".screen-content").removeClass().addClass("screen-content screen-img2");
-               });
-    });
